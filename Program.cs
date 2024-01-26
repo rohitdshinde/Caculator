007b40f7fce962a09aaad5e5bdeb6315acf878f6
rohitdshinde <157791747+rohitdshinde@users.noreply.github.com>
2024-01-26 22:32:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            int result;
+
+
+            Console.WriteLine("All method");
+            Console.WriteLine("Enter a first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Your choice a Adiition,b is substraction,Any key multiplication");
+            answer=Console.ReadLine();
+            if (answer =="a")
+            {
+                result = a + b;
+            }
+            else if (answer =="b")
+            {
+                result = a - b;
+
+            }
+            else
+            {
+                result = a * b;
+            }
+
+            Console.WriteLine("Your answer is" + result);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
